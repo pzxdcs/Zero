bcd3c5c20dca7dec1ef5f581aa0c3cebaa534b20
zxc <zhangxuecheng8@gmail.com>
2015-12-07 17:03:53 +0800

    Initial Commit

---
+++ 屏/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  屏
+//
+//  Created by qingyun on 15/12/7.
+//  Copyright © 2015年 zhangxuecheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
